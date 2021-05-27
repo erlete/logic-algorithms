4d8b58cbd2cbd978fed95ff3cef3c98264e55fde
Paulo Sanchez <szblzpaulo@gmail.com>
2021-05-27 15:39:59 +0200

    Verbose mode implementation
    Added a 'verbose' mode for easier debugging, which prints the modified values of the propositions and connectives while the algorithm is running.
    
    Added a 'determine(...)' function, which gets passed a variable and determines its value.
    
    Added a 'summary(...)' function, which takes any number of propositions and/or connectives and represents them in an organised visual table.

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ class Proposition:
         return f'{self.Name}'
 
 class Symbol(Proposition):
-    def __init__(self, proposition):
+    def __init__(self, proposition, verbose = False):
         self.Symbol = ''
         self.Propositions = (proposition,)
+        self.Verbose = verbose
 
         self.Value = True
         if self.undefinedPropositionValue():
@@ -35,27 +36,40 @@ class Symbol(Proposition):
 
     def setPropositionValue(self):
         for index in range(len(self.Propositions)):
-            print(f"Changed value for proposition:\t{self.Propositions[index]}\n\tdue to connective:\t{self.__repr__()}\n")
-            print(f'Previous value:\t{self.Propositions[index].getValue()}')
-            self.Propositions[index].setValue(self.Structure[True][index])
-            print(f'Actual value:\t{self.Propositions[index].getValue()}\n')
+            if self.Verbose:
+                print(f' Proposition [{self.Propositions[index]}] '.center(80, '-') + '\n')
+                print(f'Changed value for proposition:\t{self.Propositions[index]}')
+                print(f'due to connective(s):'.rjust(30) + f'\t{self.__repr__()}\n')
+                print(f'Previous value:\t{self.Propositions[index].getValue()}')
+            try:
+                self.Propositions[index].setValue(self.Structure[True][index])
+            except KeyError:
+                print(f'IncoherenceError: Undefined value for proposition {self.Propositions[index]}.')
+            print(f'New value:\t{self.Propositions[index].getValue()}\n') if self.Verbose else None
 
     def setConnectiveValue(self):
-        print(f"Changed value for connective:\t{self.__repr__()}\n\tdue to propositions:\t{self.Propositions[0]}: {self.Propositions[0].getValue()}\n\tand\t\t\t{self.Propositions[1]}: {self.Propositions[1].getValue()}\n")
-        print(f'Previous value:\t{self.getValue()}')
-        self.Value = self.Structure[True]
-        print(f'Actual value:\t{self.getValue()}\n')
+        if self.Verbose:
+            print(f' Connective [{self.__repr__()}] '.center(80, '-') + '\n')
+            print(f'Changed value for connective:\t{self.__repr__()}') 
+            print(f'due to proposition(s):'.rjust(29) + f'\t{self.Propositions}\n')
+            print(f'Previous value:\t{self.getValue()}')
+        try:
+            self.Value = self.Structure[True]
+        except KeyError:
+            raise IncoherenceError()
+        print(f'New value:\t{self.getValue()}\n') if self.Verbose else None
 
     def undefinedPropositionValue(self):
         return True if 'Undefined' in [proposition.getValue() for proposition in self.Propositions] else False
 
     def __repr__(self):
-        return f'{self.Symbol}{str(self.Propositions)[1:-2]}'
+        return f'({self.Symbol}{str(self.Propositions)[1:-2]})'
 
 class Not(Symbol):
-    def __init__(self, proposition):
+    def __init__(self, proposition, verbose = False):
         self.Symbol = '¬'
         self.Propositions = (proposition,)
+        self.Verbose = verbose
 
         self.Value = True
         if self.undefinedPropositionValue():
@@ -65,15 +79,16 @@ class Not(Symbol):
             self.setPropositionValue()
         else:
             self.Structure = {
-                self.Proposition[0].getValue() == True: False,
-                self.Proposition[0].getValue() == False: True
+                self.Proposition[0].getValue() == True: (False,),
+                self.Proposition[0].getValue() == False: (True,)
             }
             self.setConnectiveValue()
 
 class And(Symbol):
-    def __init__(self, proposition_1, proposition_2):
+    def __init__(self, proposition_1, proposition_2, verbose = False):
         self.Symbol = '^'
         self.Propositions = (proposition_1, proposition_2)
+        self.Verbose = verbose
 
         self.Value = True
         if self.undefinedPropositionValue():
@@ -81,7 +96,6 @@ class And(Symbol):
                 (self.getValue(), self.Propositions[0].getValue()) == (True, True): (True, True),
                 (self.getValue(), self.Propositions[1].getValue()) == (True, True): (True, True)
             }
-            print(self.Structure)
             self.setPropositionValue()
         else:
             self.Structure = {
@@ -96,9 +110,10 @@ class And(Symbol):
         return f'({self.Propositions[0]} {self.Symbol} {self.Propositions[1]})'
 
 class Or(And):
-    def __init__(self, proposition_1, proposition_2):
+    def __init__(self, proposition_1, proposition_2, verbose = False):
         self.Symbol = 'v'
         self.Propositions = (proposition_1, proposition_2)
+        self.Verbose = verbose
 
         self.Value = True
         if self.undefinedPropositionValue():
@@ -119,9 +134,10 @@ class Or(And):
             self.setConnectiveValue()
 
 class Implicative(And):
-    def __init__(self, proposition_1, proposition_2):
+    def __init__(self, proposition_1, proposition_2, verbose = False):
         self.Symbol = '⟶'
         self.Propositions = (proposition_1, proposition_2)
+        self.Verbose = verbose
 
         self.Value = True
         if self.undefinedPropositionValue():
@@ -142,9 +158,10 @@ class Implicative(And):
             self.setConnectiveValue()
 
 class BiImplicative(And):
-    def __init__(self, proposition_1, proposition_2):
+    def __init__(self, proposition_1, proposition_2, verbose = False):
         self.Symbol = '⟷'
         self.Propositions = (proposition_1, proposition_2)
+        self.Verbose = verbose
 
         self.Value = True
         if self.undefinedPropositionValue():
@@ -162,25 +179,47 @@ class BiImplicative(And):
             }
             self.setConnectiveValue()
 
-# Proposition generation (null values):
+def determine(statement):
+    return statement.getValue()
+
+def summary(*statements):
+    t1, t2, t3 = 'ID:', 'Value:', 'Element:'
+    idlen, vallen, ellen = 5, 15, 60
+    title = t1 + (idlen - len(t1)) * ' ' + t2 + (vallen - len(t2)) * ' ' + t3 + (ellen - len(t3)) * ' '
+    print('-' * 80)
+    print(title)
+    print('-' * 80)
+    for index in range(len(statements)):
+        r1 = str(index)
+        r2 = str(statements[index].getValue())
+        r3 = str(statements[index])
+        row = r1 + (idlen - len(r1)) * ' ' + r2 + (vallen - len(r2)) * ' ' + r3 + (ellen - len(r3)) * ' '
+        print(row)
+
+switch = True
+
+""" # IncoherenceError demonstration:
+
 p = Proposition('rains')
 q = Proposition('coat')
 r = Proposition('umbrella')
 
-print('-Basic proposition evaluation' + '-' * 51 + '\n')
+s1 = Symbol(p, verbose = switch)
+s2 = Not(q, verbose = switch)
+s3 = Implicative(p, And(q, r, verbose = switch))
 
-print('# Evaluating s1\n')
-s1 = Symbol(q)
+"""
 
-print('## Evaluating s2\n')
-s2 = Not(r)
+""" # Valid demonstration:
 
-print('-And + Implicative evaluation' + '-' * 51 + '\n')
+p = Proposition('rains')
+q = Proposition('home')
+r = Proposition('cafe')
 
-print('### Evaluating s3\n')
-s3 = And(q, r)
+s1 = Symbol(p, verbose = switch)
+s2 = Not(q, verbose = switch)
+s3 = Implicative(p, Or(q, r, verbose = switch), verbose = switch)
 
-print('#### Evaluating s4\n')
-s4 = BiImplicative(p, And(q, r))
+"""
 
-print('-' * 80 + '\n')
+summary(p, q, r, s1, s2, s3)
